787e1499968a021c63917743cb6df360418bd713
DHANASHRI <DHANASHRI@DHANU099>
2024-06-24 23:06:50 +0530

    Add project files.

---
+++ TestTaskEPTL_UI/Controllers/EmpController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTaskEPTL_UI.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult report()
+        {
+            return View();
+        }
+
+    }
+}
